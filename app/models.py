ce19cfd1a549b8817812e7ad9653024acf3d4a84
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2014-09-18 22:08:28 +0530

    Implemented signup without pwd hashing

--- app/models.py
+++ app/models.py
@@ -1,9 +1,22 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(150), index = True)
-	email = db.Column(db.String(150), unique = True)
-	
-	def __repr__(self):
-		return self.email
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), index=True)
+    email = db.Column(db.String(150), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return self.email
+
+    '''@property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)'''
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
-from forms import SignupForm 
-
+from forms import SignupForm
+from werkzeug.security import generate_password_hash
+from app import models, db
 
 @app.route('/')
 @app.route('/index')
@@ -12,6 +13,13 @@ def index():
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
+        user = models.User(name = form.name.data,
+                                    email = form.email.data,
+                                    password_hash = form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+
         flash('Sign Up Sucessful')
         return redirect(url_for('index'))
     return render_template('signup.html', title = 'Sign Up', form = form)
